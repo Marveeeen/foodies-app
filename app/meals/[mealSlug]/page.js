4bfbaa57619dc78afef92e583d7d9cba1a8108e9
Marveeeen <jmvroqueee@gmail.com>
2024-07-23 11:15:48 +0800

    chore: added not-found fallback page

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,10 +1,17 @@
+import Image from "next/image";
+import { notFound } from "next/navigation";
+
 import { getMeal } from "@/lib/meals";
 import classes from "./page.module.css";
-import Image from "next/image";
 
 export default function MealDetailsPage({ params }) {
   const meal = getMeal(params.mealSlug);
-  meal.instructions = meal.instructions.replace(/\n/g, '<br />')
+
+  if (!meal) {
+    notFound();
+  }
+
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
 
   return (
     <>
