c949de54328ad3ded11a035e6ddf20ccb9d62fbf
Marveeeen <jmvroqueee@gmail.com>
2024-07-23 21:30:22 +0800

    feature: implement server action

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,8 +1,17 @@
+"use client";
+
+import { useFormState } from "react-dom";
+
 import ImagePicker from "@/components/meals/image-picker";
+import MealFormSubmit from "@/components/meals/meal-form-submit";
+
+import { shareMeal } from "@/lib/actions";
 
 import classes from "./page.module.css";
 
 export default function ShareMealPage() {
+  const [formState, formAction] = useFormState(shareMeal, { message: null });
+
   return (
     <>
       <header className={classes.header}>
@@ -12,7 +21,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -41,8 +50,9 @@ export default function ShareMealPage() {
             ></textarea>
           </p>
           <ImagePicker label="Your image" name="image" />
+          {formState.message && <p>{formState.message}</p>}
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealFormSubmit />
           </p>
         </form>
       </main>
